5a6d86dfa8b46c5c49c036ab4276182ec189a3bb
Wangchou Lu <kindalu@gmail.com>
2018-03-28 16:34:28 +0800

    tuning the timing

--- prototype/index.js
+++ prototype/index.js
@@ -2,6 +2,7 @@
 // 2. install tts sound on Mac by
 //    System Preference -> Accessibility -> Speech -> 聲 -> Japanese
 // 3. make sure the network is connected for Apple Speech Recognition
+// Increase the font size for better experience
 const osa = require('osa2')
 const promptly = require('promptly')
 const chalk = require('chalk')
@@ -24,15 +25,9 @@ const say = osa((text, props) => {
 const toggleListen = osa((seconds = 0) => {
   const fnKeyCode = 63
   delay(seconds)
-  return Application('System Events').keyCode([fnKeyCode, fnKeyCode])
+  Application('System Events').keyCode([fnKeyCode, fnKeyCode])
+  return delay(0.5)
 })
-
-const enter = osa(() => {
-  const enterKeyCode = 36
-  delay(3)
-  return Application('System Events').keyCode([enterKeyCode])
-})
-
 
 const kyoko = { using: "Kyoko" }
 const otoya = { using: "Otoya" }
@@ -46,39 +41,49 @@ let trim = str => {
   return str.replace(/[！？、。]/g, '')
 }
 
+const calScore = (text, speakText) => {
+  let trimedText = trim(text)
+  let strDist = calcDist(speakText, trimedText)
+  let len = max(trimedText.length, speakText.length)
+  return Math.trunc(max(len - strDist, 0) * 100 / len)
+}
+
 async function learn (item, isFirstTime = true) {
   if(isFirstTime) {
     gray('--------------------------------------------\n')
     white(repeatMe)
+    await say(repeatMe, meijia)
   } else {
     white(repeatMeSecond)
   }
 
-  blue(item.text + '\n')
-  await toggleListen()
+  blue(' ' + item.text + '\n')
+
+  // Key Part Start //////////////////////////////////////////////////////////////
+  let speakingRate = isFirstTime ? 160 : 120
   let speaker = {
     using: item.gender === 'f' ? "Kyoko" : "Otoya",
-    speakingRate: isFirstTime ? 160 : 120
+    speakingRate
   }
   say(item.text, speaker)
-  const speakText = await promptly.prompt('換你說：')
+  const delayTimeInSeconds = item.text.length/(speakingRate/60) - 1
+  await toggleListen(delayTimeInSeconds)
+  const speakText = await promptly.prompt('嗶聲後說：')
   await toggleListen()
+  // Key Part End //////////////////////////////////////////////////////////////
 
   // press the enter after recognition when prototyping
-  let trimedText = trim(item.text)
-  let strDist = calcDist(speakText, trimedText)
-  let len = max(trimedText.length, speakText.length)
-  let score = Math.trunc(max(len - strDist, 0) * 100 / len)
+  const score = calScore(item.text, speakText)
   if(score >= 80) {
-    let goodWord = score === 100 ? "すごい" : "いいね"
+    let goodWord = score === 100 ? "パーフェクト" : "いいね"
     green(`${score}点: ${goodWord}\n`)
     await say(goodWord, kyoko)
   } else if(isFirstTime) {
-    red(`${score}点: ${strDist}/${len} もう一回\n`)
+    red(`${score}点: もう一回\n`)
     await say('もう一回', otoya)
     await learn(item, false)
   } else {
-    red(`${score}点: ${strDist}/${len} やっぱりだめだ\n`)
+    red(`${score}点: やっぱりだめだ\n`)
     await say('やっぱりだめだ', otoya)
   }
 }
@@ -86,26 +91,23 @@ async function learn (item, isFirstTime = true) {
 const items = [
   {text: "こんにちは", gender: 'm'},
   {text: "なぜですか？", gender: 'f'},
-  {text: "そりゃ無理だ", gender: 'm'},
-  {text: "おね様", gender: 'f'},
+  {text: "どうしましたか？", gender: 'm'},
+  {text: "おねさま", gender: 'f'},
   {text: "真実はいつもひとつ！", gender: 'm'},
-  {text: "私、気になります！", gender: 'f'},
+  {text: "わたし、気になります！", gender: 'f'},
   {text: "おまえはもう死んでる", gender: 'm'},
   {text: "わーい！たーのしー！すごい！", gender: 'f'},
   {text: "はじめまして", gender: 'm'},
-  {text: "不愉快です", gender: 'f'},
+  {text: "頑張ります！", gender: 'f'},
   {text: "はい、わかりました", gender: 'm'},
   {text: "うるさい、うるさい！", gender: 'f'},
   {text: "どなたですか", gender: 'm'},
   {text: "あんたバカ？", gender: 'f'},
-  {text: "どうしましたか？", gender: 'm'},
-  {text: "頑張ります！", gender: 'f'},
 ]
 
 const main = async () => {
   for(let i = 0; i < items.length; i++) {
     await learn(items[i])
-    // console.log(trim(items[i].text))
     /*
     let item = items[i]
     let speaker = {
