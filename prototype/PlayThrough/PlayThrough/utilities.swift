417bee0db72fdd88bd26761733935dc87ca98539
Wangchou Lu <kindalu@gmail.com>
2018-04-20 12:24:39 +0800

    add edit distance function

--- prototype/PlayThrough/PlayThrough/utilities.swift
+++ prototype/PlayThrough/PlayThrough/utilities.swift
@@ -53,6 +53,82 @@ func configureAudioSession() {
     }
 }
 
+// EditDistance from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Swift
+/**
+ * Levenshtein edit distance calculator
+ *
+ * Inspired by https://gist.github.com/bgreenlee/52d93a1d8fa1b8c1f38b
+ * Improved with http://stackoverflow.com/questions/26990394/slow-swift-arrays-and-strings-performance
+ */
+
+func min3(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    return min( min(a, c), min(b, c))
+}
+
+class Array2D {
+    var cols:Int, rows:Int
+    var matrix: [Int]
+    
+    init(cols:Int, rows:Int) {
+        self.cols = cols
+        self.rows = rows
+        matrix = Array(repeating:0, count:cols*rows)
+    }
+    
+    subscript(col:Int, row:Int) -> Int {
+        get {
+            return matrix[cols * row + col]
+        }
+        set {
+            matrix[cols*row+col] = newValue
+        }
+    }
+    
+    func colCount() -> Int {
+        return self.cols
+    }
+    
+    func rowCount() -> Int {
+        return self.rows
+    }
+}
+
+func distanceBetween(_ aStr: String, _ bStr: String) -> Int {
+    let a = Array(aStr.utf16)
+    let b = Array(bStr.utf16)
+    
+    if(a.count == 0 || b.count == 0) {
+        return a.count + b.count
+    }
+    
+    let dist = Array2D(cols: a.count + 1, rows: b.count + 1)
+    
+    for i in 1...a.count {
+        dist[i, 0] = i
+    }
+    
+    for j in 1...b.count {
+        dist[0, j] = j
+    }
+    
+    for i in 1...a.count {
+        for j in 1...b.count {
+            if a[i-1] == b[j-1] {
+                dist[i, j] = dist[i-1, j-1]  // noop
+            } else {
+                dist[i, j] = min3(
+                    dist[i-1, j] + 1,  // deletion
+                    dist[i, j-1] + 1,  // insertion
+                    dist[i-1, j-1] + 1  // substitution
+                )
+            }
+        }
+    }
+    
+    return dist[a.count, b.count]
+}
+
+
 // Latency tested result
 // AVAudioEngine ~= 5.41ms
 // AudioKit ~= 16.25ms
