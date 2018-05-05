d86dcb194eb5ee1b46428faa225e7be9111af830
Wangchou Lu <kindalu@gmail.com>
2018-05-05 10:24:53 +0800

    refactoring EventCenter

--- prototype/VoiceOnly/VoiceOnly/Commands/EventCenter.swift
+++ prototype/VoiceOnly/VoiceOnly/Commands/EventCenter.swift
@@ -15,74 +15,69 @@ func postEvent (_ type: EventType, _ object: Any) {
     NotificationCenter.default.post(name: type, object: object)
 }
 
-fileprivate typealias EventDelegate = EventCenter
-
-fileprivate extension Selector {
-    static let sayStarted = #selector(EventDelegate.onSayStarted(_:))
-    static let stringSaid = #selector(EventDelegate.onStringSaid(_:))
-    static let sayEnded = #selector(EventDelegate.onSayEnded(_:))
-    static let listenStarted = #selector(EventDelegate.onListenEnded(_:))
-    static let stringRecognized = #selector(EventDelegate.onStringRecognized(_:))
-    static let listenEnded = #selector(EventDelegate.onListenEnded(_:))
-    static let scoreCalculated = #selector(EventDelegate.onScoreCalculated(_:))
-    static let gameStateChanged = #selector(EventDelegate.onGameStateChanged(_:))
-}
-
 class EventCenter {
     static let shared = EventCenter()
-    
-    init() {
-        observe(.sayStarted, .sayStarted)
-        observe(.stringSaid, .stringSaid)
-        observe(.sayEnded, .sayEnded)
-        observe(.listenStarted, .listenStarted)
-        observe(.stringRecognized, .stringRecognized)
-        observe(.listenEnded, .listenEnded)
-        observe(.scoreCalculated, .scoreCalculated)
-        observe(.gameStateChanged, .gameStateChanged)
-    }
-    
-    func observe(_ type: EventType, _ eventHandler: Selector) {
+
+    func observe(_ type: EventType) {
         NotificationCenter.default.addObserver(
             self,
-            selector: eventHandler,
+            selector: #selector(onAll(_:)),
             name: type,
             object: nil
         )
     }
     
+    init() {
+        observe(.stringSaid)
+        observe(.sayEnded)
+        observe(.listenStarted)
+        observe(.stringRecognized)
+        observe(.listenEnded)
+        observe(.scoreCalculated)
+        observe(.gameStateChanged)
+    }
+    
+    @objc func onAll(_ event: Event) {
+        switch event.name {
+        case .gameStateChanged:
+            onGameStateChanged(event)
+        default:
+            return
+        }
+    }
+    
     // SayCommand
-    @objc func onSayStarted(_ event: Event) {
+    func onSayStarted(_ event: Event) {
         (event.object as! SayCommand).log()
     }
     
-    @objc func onStringSaid(_ event: Event) {
+    func onStringSaid(_ event: Event) {
         print(event.object as! String, terminator: "")
     }
     
-    @objc func onSayEnded(_ event: Event) {
+    func onSayEnded(_ event: Event) {
         print("")
     }
     
     // ListenCommand
-    @objc func onListenStarted(_ event: Event) {
+    func onListenStarted(_ event: Event) {
         print("hear <<< ", terminator: "")
     }
     
-    @objc func onStringRecognized(_ event: Event) {
+    func onStringRecognized(_ event: Event) {
         //print(event.object as! String, terminator: "")
     }
     
-    @objc func onListenEnded(_ event: Event) {
+    func onListenEnded(_ event: Event) {
         print(event.object as! String)
     }
     
     // onScore
-    @objc func onScoreCalculated(_ event: Event) {
+    func onScoreCalculated(_ event: Event) {
         print("score calculated: \(event.object as! Int)")
     }
     
-    @objc func onGameStateChanged(_ event: Event) {
+    func onGameStateChanged(_ event: Event) {
         print("= game state changed: \(event.object as! GameState) =")
     }
 }
