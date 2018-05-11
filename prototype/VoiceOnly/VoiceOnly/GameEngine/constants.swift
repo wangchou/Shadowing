ad8066bb11ff59497f2caa122ea8280e3cbe65d5
Wangchou Lu <kindalu@gmail.com>
2018-05-11 22:18:25 +0800

    reorganize structure

--- prototype/VoiceOnly/VoiceOnly/GameEngine/constants.swift
+++ prototype/VoiceOnly/VoiceOnly/GameEngine/constants.swift
@@ -15,13 +15,11 @@ let Kyoko = "com.apple.ttsbundle.Kyoko-premium"
 let Oren = "com.apple.ttsbundle.siri_female_ja-JP_compact"
 let Hattori = "com.apple.ttsbundle.siri_male_ja-JP_compact"
 
-let HARUHI = "ただの人間には興味ありません。この中に宇宙人、未来人、異世界人、超能力者がいたら、あたしのところに来なさい。以上。"
-
 let normalRate = AVSpeechUtteranceDefaultSpeechRate
-let slowestRate = AVSpeechUtteranceDefaultSpeechRate * 0.6
 
 // it's bad when monitoring at street
 // if in quite place. turn micOutVolume to 3
 let micOutVolume: Float = 0
+
 let screenSize = UIScreen.main.bounds
 
