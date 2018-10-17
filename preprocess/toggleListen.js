f202cc7265d575d95a278aec588f39b4409ef60d
Wangchou Lu <kindalu@gmail.com>
2018-10-17 13:46:10 +0800

    remove unused files

--- preprocess/toggleListen.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/osascript -l JavaScript
-var app = Application.currentApplication()
-app.includeStandardAdditions = true
-
-const fnKeyCode = 63
-Application('System Events').keyCode([fnKeyCode, fnKeyCode])
-delay(0.5)
