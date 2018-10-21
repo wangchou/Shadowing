a5835da6fd97657d9bfbb2683c9c9f9497eae6d9
Wangchou Lu <kindalu@gmail.com>
2018-10-21 14:37:20 +0800

    fix app code signing bug when upload to App Store

---
+++ iOS/prepareForCodeSigning.sh
@@ -0,0 +1,16 @@
+# https://github.com/fastlane/fastlane/issues/13401
+lipo -remove x86_64 Carthage/Build/iOS/Alamofire.framework/Alamofire -o Carthage/Build/iOS/Alamofire.framework/Alamofire
+lipo -remove i386 Carthage/Build/iOS/Alamofire.framework/Alamofire -o Carthage/Build/iOS/Alamofire.framework/Alamofire
+lipo -remove x86_64 Carthage/Build/iOS/Charts.framework/Charts -o Carthage/Build/iOS/Charts.framework/Charts
+lipo -remove i386 Carthage/Build/iOS/Charts.framework/Charts -o Carthage/Build/iOS/Charts.framework/Charts
+lipo -remove x86_64 Carthage/Build/iOS/FBLPromises.framework/FBLPromises -o Carthage/Build/iOS/FBLPromises.framework/FBLPromises
+lipo -remove i386 Carthage/Build/iOS/FBLPromises.framework/FBLPromises -o Carthage/Build/iOS/FBLPromises.framework/FBLPromises
+lipo -remove x86_64 Carthage/Build/iOS/Promises.framework/Promises -o Carthage/Build/iOS/Promises.framework/Promises
+lipo -remove i386 Carthage/Build/iOS/Promises.framework/Promises -o Carthage/Build/iOS/Promises.framework/Promises
+lipo -remove x86_64 Carthage/Build/iOS/SQLite.framework/SQLite -o Carthage/Build/iOS/SQLite.framework/SQLite
+lipo -remove i386 Carthage/Build/iOS/SQLite.framework/SQLite -o Carthage/Build/iOS/SQLite.framework/SQLite
+lipo -remove x86_64 Carthage/Build/iOS/SwiftIconFont.framework/SwiftIconFont -o Carthage/Build/iOS/SwiftIconFont.framework/SwiftIconFont
+lipo -remove i386 Carthage/Build/iOS/SwiftIconFont.framework/SwiftIconFont -o Carthage/Build/iOS/SwiftIconFont.framework/SwiftIconFont
+lipo -remove x86_64 Carthage/Build/iOS/SwiftyJSON.framework/SwiftyJSON -o Carthage/Build/iOS/SwiftyJSON.framework/SwiftyJSON
+lipo -remove i386 Carthage/Build/iOS/SwiftyJSON.framework/SwiftyJSON -o Carthage/Build/iOS/SwiftyJSON.framework/SwiftyJSON
+
