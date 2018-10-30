6ce6dc946d5e84259582e60cbe88c35cfb2a0066
Wangchou Lu <kindalu@gmail.com>
2018-10-31 01:55:49 +0800

    add ForeignDev target
    watch https://www.youtube.com/watch?v=f4VWv_J9HgA&frags=pl%2Cwn
    - add new target
    - reuse info.plist
    - add -D ForeignDev flag
    - add scheme

--- iOS/Project/AppDelegate.swift
+++ iOS/Project/AppDelegate.swift
@@ -26,6 +26,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = RootContainerViewController()
         window?.makeKeyAndVisible()
 
+        #if ForeignDev
+            print("== 多國語言開發版 ==")
+        #else
+            print("== 日本語版 ==")
+        #endif
+
         return true
     }
 
