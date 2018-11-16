3a5382e30e19e566316c1727f51c21215eaa2ba1
Wangchou Lu <kindalu@gmail.com>
2018-11-16 19:16:05 +0800

    simplify icList tableCell

--- iOS/UI/InfiniteChanllengeMode/InfiniteChallengeTableCell.swift
+++ iOS/UI/InfiniteChanllengeMode/InfiniteChallengeTableCell.swift
@@ -12,7 +12,6 @@ private let i18n = I18n.shared
 
 class InfiniteChallengeTableCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var syllablesCountLabel: UILabel!
     @IBOutlet weak var finishedRateLabel: UILabel!
     @IBOutlet weak var rankLabel: UILabel!
 
@@ -20,7 +19,6 @@ class InfiniteChallengeTableCell: UITableViewCell {
         willSet(level) {
             titleLabel.text = level.title
             titleLabel.textColor = level.color
-            syllablesCountLabel.text = "\(i18n.syllablesCount):\(level.minSyllablesCount)~\(level.maxSyllablesCount)"
             rankLabel.attributedText = getRankText(string: level.bestInfinteChallengeRank)
             finishedRateLabel.attributedText = getProgressText(string: level.bestInfinteChallengeProgress)
         }
