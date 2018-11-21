a0939d08fef8c6aacdcd13acdb76a0179c83a097
Wangchou Lu <kindalu@gmail.com>
2018-11-21 17:24:16 +0800

    add timeline to icPage

--- iOS/UI/InfiniteChanllengeMode/ICListTopView.swift
+++ iOS/UI/InfiniteChanllengeMode/ICListTopView.swift
@@ -19,6 +19,10 @@ class ICListTopView: UIView, GridLayout, ReloadableView {
 
     var spacing: CGFloat = 0
 
+    var timelineColumnCount: Int  = 15
+    var timelineYPadding: CGFloat = 0.3
+    var timelineXPadding: CGFloat = -0.35
+
     let animationSecs: TimeInterval = 0.5
 
     var percent: Float = 0
@@ -152,6 +156,7 @@ class ICListTopView: UIView, GridLayout, ReloadableView {
 // Daily Goal Mode
 extension ICListTopView {
     func renderDailyGoalMode() {
+        gridCount = 48
         updateDailyViewBGColor()
 
         let circleFrame = getFrame(11, 3, 24, 24)
@@ -244,13 +249,115 @@ extension ICListTopView {
 // Timeline Mode
 extension ICListTopView {
     func renderTimelineMode() {
+        gridCount = 16
         backgroundColor = longTermGoalColor.withSaturation(0.3)
+        addTimeline()
+        addTimelineBottomBar()
+    }
+
+    func addTimeline() {
+        let dailyGoal = context.gameSetting.dailySentenceGoal
+        let today = Date()
+        let weekday = Calendar.current.component(.weekday, from: today)
+        var timelineBox = TimelineBox(date: today, row: weekday, column: 1)
+        //let recordsByDate = getRecordsByDate()
+        var index = 0
+        let sentencesCounts = getSentenceCountsByDays()
+        while timelineBox.column < timelineColumnCount {
+            //let dateString = getDateKey(date: timelineBox.date)
+            //let color = getColorFrom(records: recordsByDate[dateString])
+            let isGoalCompleted = (index < sentencesCounts.count && sentencesCounts[index] >= dailyGoal)
+            addTimelineBox(
+                row: timelineBox.row,
+                column: timelineBox.column,
+                color: isGoalCompleted ? .black : .clear
+            )
+            timelineBox.toYesterday()
+            if timelineBox.day == 1 {
+                addTimelineTextLabel(row: 0, column: timelineBox.column, text: "\(timelineBox.month)月")
+            }
+            index += 1
+        }
+        let weekdayLabels = ["日", "月", "火", "水", "木", "金", "土"]
+        for i in 0..<weekdayLabels.count {
+            addTimelineTextLabel(row: i + 1, column: 0, text: weekdayLabels[i], color: (i == 0 || i == 6) ? .red : .black)
+        }
+    }
+
+    func addTimelineBottomBar() {
+        let topTexts = ["\(continues)", "\(bestCount)", "24%", "54%", "3211"]
+        let bottomTexts = ["連続", "ベスト", "過去7日間", "過去30日間", "文"]
+        for boxIndex in 0..<5 {
+            let box = addRect(x: 1 + boxIndex * 3, y: 8, w: 3, h: 2)
+            addTimelinePadding(box)
+            box.frame.size.width -= stepFloat * 0.3
+            box.frame.size.height += stepFloat * 0.2
+            box.frame.origin.y += stepFloat * 0.3
+            box.roundBorder(borderWidth: 0, cornerRadius: 5, color: .clear)
+            box.backgroundColor = .white
+
+            let topText = addText(
+                x: 1 + boxIndex * 3, y: 8, w: 4, h: 2,
+                text: topTexts[boxIndex],
+                font: MyFont.bold(ofSize: stepFloat * 0.7),
+                color: rgb(74, 74, 74)
+            )
+            topText.textAlignment = .center
+            topText.centerX(box.frame)
+            topText.frame.origin.y += stepFloat * 0.4
+
+            let bottomText = addText(
+                x: 1 + boxIndex * 3, y: 9, w: 4, h: 2,
+                text: bottomTexts[boxIndex],
+                font: MyFont.regular(ofSize: stepFloat * 0.5),
+                color: .lightGray
+            )
+            bottomText.textAlignment = .center
+            bottomText.centerX(box.frame)
+            bottomText.frame.origin.y += stepFloat * 0.2
+        }
+    }
+
+    func addTimelineBox(row: Int, column: Int, color: UIColor = myLightText) {
+        let width = stepFloat * 0.7
+        let box = UIView()
+        box.backgroundColor = color
+        layout(timelineColumnCount - column, row, 1, 1, box)
+        box.frame.size.width = width
+        box.frame.size.height = width
+        box.roundBorder(borderWidth: 1, cornerRadius: width/2, color: .black)
+        addSubview(box)
+
+        addTimelinePadding(box)
+    }
+
+    func addTimelineTextLabel(row: Int, column: Int, text: String, color: UIColor = .black) {
+        let label = addText(
+            x: timelineColumnCount - column,
+            y: row,
+            w: 3, h: 1,
+            text: text,
+            font: MyFont.thin(ofSize: stepFloat * 0.7),
+            color: color
+        )
+        if row == 0 { // month labels
+            label.frame.origin.y += stepFloat * (-0.1 + timelineYPadding)
+        } else {
+            label.frame.origin.y += stepFloat * (-0.2 + timelineYPadding)
+        }
+        label.frame.origin.x += stepFloat * timelineXPadding
+    }
+
+    func addTimelinePadding(_ view: UIView) {
+        view.frame.origin.y += stepFloat * timelineYPadding
+        view.frame.origin.x += stepFloat * timelineXPadding
     }
 }
 
 // LongTermGoalMode
 extension ICListTopView {
     func renderLongTermGoalMode() {
+        gridCount = 48
         backgroundColor = rgb(28, 28, 28)
     }
 }
