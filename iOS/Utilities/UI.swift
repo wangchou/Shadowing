3d2d8f20e31f9c5067e3a6996888d6e975273787
Wangchou Lu <kindalu@gmail.com>
2019-01-10 13:11:20 +0800

    add uiview to image

--- iOS/Utilities/UI.swift
+++ iOS/Utilities/UI.swift
@@ -106,6 +106,23 @@ extension UIView {
         }
     }
 
+    // https://stackoverflow.com/questions/30696307/how-to-convert-a-uiview-to-an-image
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: bounds)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContext(self.frame.size)
+            self.layer.render(in:UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return UIImage(cgImage: image!.cgImage!)
+        }
+    }
 }
 
 extension UIScrollView {
