62d0087947f4874a6ee8be785510f26b1b7fe132
Wangchou Lu <kindalu@gmail.com>
2019-04-27 21:45:23 +0900

    adjust english sentence difficulties

--- iOS/Model/Level.swift
+++ iOS/Model/Level.swift
@@ -14,8 +14,8 @@ var avgKanaCountDict: [String: Float] = [:]
 private let minKanaCounts = [2, 7, 10, 12, 14, 16, 19, 23, 27, 32]
 private let maxKanaCounts = [6, 9, 11, 13, 15, 18, 22, 26, 31, 40]
 //for en
-private let minSyllablesCounts = [1, 4, 6, 8, 10, 12, 14, 17, 20, 24]
-private let maxSyllablesCounts = [3, 5, 7, 9, 11, 13, 16, 19, 23, 30]
+private let minSyllablesCounts = [1, 4, 6, 8, 10, 12, 14, 16, 18, 22]
+private let maxSyllablesCounts = [3, 5, 7, 9, 11, 13, 15, 17, 21, 30]
 
 private let colors = [myRed, myRed, myOrange, myOrange, myGreen, myGreen, myBlue, myBlue, myPurple, myPurple]
 private let titles = ["入門一", "入門二", "初級一", "初級二",
