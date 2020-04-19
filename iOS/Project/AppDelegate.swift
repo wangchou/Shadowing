f08d0bd0b0ed4d8f4930f3234be70cec29702867
Wangchou Lu <kindalu@gmail.com>
2020-04-19 08:12:02 +0800

    update firebase to fix call ui on bg thread

--- iOS/Project/AppDelegate.swift
+++ iOS/Project/AppDelegate.swift
@@ -41,17 +41,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        postCommand(.forceStopGame)
+        if let messenger = UIApplication.getPresentedViewController() as? Messenger {
+            messenger.dismiss(animated: false)
+        }
     }
 
     func applicationDidEnterBackground(_: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        SpeechEngine.shared.stop()
     }
 
     func applicationWillEnterForeground(_: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        rootViewController.updateWhenEnterForeground()
+        DispatchQueue.main.async { [weak self] in
+            self?.rootViewController.updateWhenEnterForeground()
+        }
         SpeechEngine.shared.restart()
     }
 
