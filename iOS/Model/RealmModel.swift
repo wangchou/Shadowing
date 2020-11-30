2afa27be2336d1d4b29cb85fa64498b75d93aeff
Wangchou Lu <kindalu@gmail.com>
2020-11-30 16:14:08 +0800

    generate sidefile

---
+++ iOS/Model/RealmModel.swift
@@ -0,0 +1,60 @@
+//
+//  RealmModel.swift
+//  hanashitai
+//
+//  Created by Wangchou Lu on R 2/11/30.
+//  Copyright © Reiwa 2 Lu, WangChou. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RMSentence: Object {
+    @objc dynamic var id = 0
+    @objc dynamic var ja = ""
+    @objc dynamic var en = ""
+    @objc dynamic var cmn = ""
+    @objc dynamic var ja_tts_fixes = ""
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+class RMStringToId: Object {
+    @objc dynamic var origin = ""
+    @objc dynamic var id = 0
+
+    override static func primaryKey() -> String? {
+        return "origin"
+    }
+}
+
+class RMJaInfo: Object {
+    @objc dynamic var difficulty = 0
+    @objc dynamic var ids = ""
+
+    override static func primaryKey() -> String? {
+        return "difficulty"
+    }
+}
+
+class RMEnInfo: Object {
+    @objc dynamic var difficulty = 0
+    @objc dynamic var ids = ""
+
+    override static func primaryKey() -> String? {
+        return "difficulty"
+    }
+}
+
+class RMTokenInfos: Object {
+    @objc dynamic var ja = ""
+    @objc dynamic var kanaCount = 0
+    @objc dynamic var tokenInfos = ""
+
+    override static func primaryKey() -> String? {
+        return "ja"
+    }
+
+}
--- iOS/preprocess/preprocess/main.swift
+++ iOS/preprocess/preprocess/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Promises
 import SwiftSyllables
-import SQLite
+import RealmSwift
 
 func getSentences() -> [[String]] {
     let file = "sentences.tsv"
@@ -37,40 +37,27 @@ print(rows.count)
 // checkKanaFixes(rows: rows)
 // checkTTSFixes(rows: rows)
 
-private var sqliteFileName = "sentences20201118.sqlite"
-
-private var writableDBPath = ""
-var dbW: Connection!
+var realm: Realm!
 
 func createWritableDB() {
-    let fileManager = FileManager.default
     do {
-        writableDBPath = try fileManager
-            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-            .appendingPathComponent("\(sqliteFileName)")
-            .path
-
-        if fileManager.fileExists(atPath: writableDBPath) {
-            try fileManager.removeItem(atPath: writableDBPath)
-        }
-        dbW = try Connection(writableDBPath, readonly: false)
+        realm = try Realm()
     } catch {
-        print("\(error)")
+        print("db update error: \(error)")
     }
 }
+
 var isAddedToTokenInfo: [String: Bool] = [:]
 func updateIdWithListened(ja: String, kanaCount: Int, tokenInfos: [[String]]) {
     guard isAddedToTokenInfo[ja] != true else { return }
     do {
-        let table = Table(tokenInfosTableName)
-        let dbJa = Expression<String>("ja")
-        let dbKanaCount = Expression<Int>("kana_count")
-        let dbTokenInfos = Expression<String>("tokenInfos")
-
-        let insertSql = table.insert(dbJa <- ja,
-                                     dbKanaCount <- kanaCount,
-                                     dbTokenInfos <- tokenInfosToString(tokenInfos: tokenInfos))
-        try dbW.run(insertSql)
+        let rmTokenInfos = RMTokenInfos()
+        rmTokenInfos.ja = ja
+        rmTokenInfos.kanaCount = kanaCount
+        rmTokenInfos.tokenInfos = tokenInfosToString(tokenInfos: tokenInfos)
+        try realm.write {                  // 新增資料
+            realm.add(rmTokenInfos)
+        }
         isAddedToTokenInfo[ja] = true
     } catch {
         print("db update error: \(error)")
@@ -85,34 +72,20 @@ func waitForFinishing() {
 }
 
 func addTokenInfosTable() {
-    do {
-        let tokenInfosTable = Table(tokenInfosTableName)
-        let ja = Expression<String>("ja")
-        let kanaCount = Expression<Int>("kana_count")
-        let tokenInfos = Expression<String>("tokenInfos")
-        try dbW.run(tokenInfosTable.drop(ifExists: true))
-        try dbW.run(tokenInfosTable.create { t in
-            t.column(ja, primaryKey: true)
-            t.column(kanaCount)
-            t.column(tokenInfos)
-        })
-        var sentences: [String] = []
-        rawDataSets.forEach { sArray in
-            sentences.append(contentsOf: sArray)
+    var sentences: [String] = []
+    rawDataSets.forEach { sArray in
+        sentences.append(contentsOf: sArray)
+    }
+    print(sentences.count)
+    for jpn in sentences {
+        isFinished = false
+        getKana(jpn, originalString: jpn).then { kana in
+            updateIdWithListened(ja: jpn,
+                                 kanaCount: kana.count,
+                                 tokenInfos: kanaTokenInfosCacheDictionary[jpn] ?? [])
+            isFinished = true
         }
-        print(sentences.count)
-        for jpn in sentences {
-            isFinished = false
-            getKana(jpn, originalString: jpn).then { kana in
-                updateIdWithListened(ja: jpn,
-                                    kanaCount: kana.count,
-                                    tokenInfos: kanaTokenInfosCacheDictionary[jpn] ?? [])
-                isFinished = true
-            }
-            waitForFinishing()
-        }
-    } catch {
-        print("db error 4: \(error)")
+        waitForFinishing()
     }
 }
 
@@ -125,69 +98,46 @@ var isAddToTranslation: [String: Bool] = [:]
 private var topicSentenceIdStart = 1000000
 
 func addStringToIdTable() {
-    let translationTable = Table(stringToIdTableName)
-    let dbOrigin = Expression<String>("origin")
-    let dbId = Expression<Int>("Id")
-    do {
-        try dbW.run(translationTable.drop(ifExists: true))
-        try dbW.run(translationTable.create { t in
-            t.column(dbOrigin, primaryKey: true)
-            t.column(dbId)
-        })
-        func addTranslation(origin: String, id: Int) {
-            do {
-                guard isAddToTranslation[origin] == nil else { return }
-                let insertSql = translationTable.insert(
-                    dbOrigin <- origin,
-                    dbId <- id
-                )
-                try dbW.run(insertSql)
-                isAddToTranslation[origin] = true
-            } catch {
-                print(error)
-            }
-        }
+    func addTranslation(origin: String, id: Int) {
+        do {
+            guard isAddToTranslation[origin] == nil else { return }
+            let rmStringToId = RMStringToId()
+            rmStringToId.origin = origin
+            rmStringToId.id = id
 
-        for (id, row) in rows.enumerated() {
-            let jpn = row[0]
-            let eng = row[1]
-            if jpnOK[jpn] == true {
-                addTranslation(origin: jpn, id: id)
+            try realm.write {
+                realm.add(rmStringToId)
             }
-            if engOK[eng] == true {
-                addTranslation(origin: eng, id: id)
-            }
-        }
 
-        // topic sentence
-        var topicSentenceId = topicSentenceIdStart
-        for sArray in rawDataSets {
-            for topicSentence in sArray {
-                topicSentenceId += 1
-                addTranslation(origin: topicSentence, id: topicSentenceId)
-            }
+            isAddToTranslation[origin] = true
+        } catch {
+            print(error)
         }
-    } catch {
-        print(error)
+    }
+
+    for (id, row) in rows.enumerated() {
+        let jpn = row[0]
+        let eng = row[1]
+        if jpnOK[jpn] == true {
+            addTranslation(origin: jpn, id: id)
+        }
+        if engOK[eng] == true {
+            addTranslation(origin: eng, id: id)
+        }
+    }
+
+    // topic sentence
+    var topicSentenceId = topicSentenceIdStart
+    for sArray in rawDataSets {
+        for topicSentence in sArray {
+            topicSentenceId += 1
+            addTranslation(origin: topicSentence, id: topicSentenceId)
+        }
     }
 }
 
 func addSentencesTable() {
     do {
-        let sentencesTable = Table(sentencesTableName)
-        let dbId = Expression<Int>("id")
-        let dbJa = Expression<String>("ja")
-        let dbEn = Expression<String>("en")
-        let dbCmn = Expression<String>("cmn")
-        let dbJaTTSFixes = Expression<String>("ja_tts_fixes")
-        try dbW.run(sentencesTable.drop(ifExists: true))
-        try dbW.run(sentencesTable.create { t in
-            t.column(dbId, primaryKey: true)
-            t.column(dbJa)
-            t.column(dbEn)
-            t.column(dbCmn)
-            t.column(dbJaTTSFixes)
-        })
         for (id, row) in rows.enumerated() {
             //if(i % 500 == 0) {
                 print(id)
@@ -241,14 +191,17 @@ func addSentencesTable() {
             isFinished = false
             getKana(jpn0, isFuri: true, originalString: jpn0)
                 .then { kana in
-                    let insertSql = sentencesTable.insert(
-                        dbId <- id,
-                        dbJa <- jpn0,
-                        dbEn <- eng0,
-                        dbCmn <- cmn,
-                        dbJaTTSFixes <- ttsFixes
-                    )
-                    try dbW.run(insertSql)
+
+                    let rmSentence = RMSentence()
+                    rmSentence.id = id
+                    rmSentence.ja = jpn0
+                    rmSentence.en = eng0
+                    rmSentence.cmn = cmn
+                    rmSentence.ja_tts_fixes = ttsFixes
+
+                    try realm.write {
+                        realm.add(rmSentence)
+                    }
                     jpnDifficulty[jpn0] = kana.count
 
                     updateIdWithListened(ja: jpn0,
@@ -263,14 +216,16 @@ func addSentencesTable() {
         for sArray in rawDataSets {
             for topicSentence in sArray {
                 topicSentenceId += 1
-                let insertSql = sentencesTable.insert(
-                    dbId <- topicSentenceId,
-                    dbJa <- topicSentence,
-                    dbEn <- "",
-                    dbCmn <- topicTranslation[topicSentence] ?? "",
-                    dbJaTTSFixes <- ""
-                )
-                try dbW.run(insertSql)
+                let rmSentence = RMSentence()
+                rmSentence.id = topicSentenceId
+                rmSentence.ja = topicSentence
+                rmSentence.en = ""
+                rmSentence.cmn = topicTranslation[topicSentence] ?? ""
+                rmSentence.ja_tts_fixes = ""
+
+                try realm.write {
+                    realm.add(rmSentence)
+                }
             }
         }
     } catch {
@@ -279,94 +234,65 @@ func addSentencesTable() {
 }
 
 func addJpInfoTables() {
-    do {
-        let jpInfoTable = Table(jpInfoTableName)
-        let dbDifficulty = Expression<Int>("difficulty")
-        let dbIds = Expression<String>("ids")
-        try dbW.run(jpInfoTable.drop(ifExists: true))
-        try dbW.run(jpInfoTable.create { t in
-            t.column(dbDifficulty, primaryKey: true)
-            t.column(dbIds)
-        })
-
-        var isAdded: [String: Bool] = [:]
-        var difficultyIds: [Int: [Int]] = [:]
-        for difficulty in 0 ... 1000 {
-            difficultyIds[difficulty] = []
-        }
-        for (id, row) in rows.enumerated() {
-            let jpn = row[0]
-            if let isScoreOK = jpnOK[jpn],
-               isScoreOK,
-               isAdded[jpn] == nil,
-               let difficulty = jpnDifficulty[jpn] {
-                isAdded[jpn] = true
-                difficultyIds[difficulty]?.append(id)
-            }
-        }
-        var totalJpCount = 0
-        print("- jpn difficulty -")
-        for difficulty in 0 ... 1000 {
-            if let ids = difficultyIds[difficulty],
-               !ids.isEmpty {
-                let insertSql = jpInfoTable.insert(
-                    dbDifficulty <- difficulty,
-                    dbIds <- encode(ids) ?? ""
-                )
-                print(difficulty, ids.count)
-                totalJpCount += ids.count
-                try dbW.run(insertSql)
-            }
-        }
-        print("totalJpCount: \(totalJpCount)")
-    } catch {
-        print("db error 6: \(error)")
+    var isAdded: [String: Bool] = [:]
+    var difficultyIds: [Int: [Int]] = [:]
+    for difficulty in 0 ... 1000 {
+        difficultyIds[difficulty] = []
     }
+    for (id, row) in rows.enumerated() {
+        let jpn = row[0]
+        if let isScoreOK = jpnOK[jpn],
+           isScoreOK,
+           isAdded[jpn] == nil,
+           let difficulty = jpnDifficulty[jpn] {
+            isAdded[jpn] = true
+            difficultyIds[difficulty]?.append(id)
+        }
+    }
+    var totalJpCount = 0
+    print("- jpn difficulty -")
+    for difficulty in 0 ... 1000 {
+        if let ids = difficultyIds[difficulty],
+           !ids.isEmpty {
+            let jpInfo = RMJaInfo()
+            jpInfo.difficulty = difficulty
+            jpInfo.ids = encode(ids) ?? ""
+            print(difficulty, ids.count)
+            totalJpCount += ids.count
+        }
+    }
+    print("totalJpCount: \(totalJpCount)")
 }
-func addEnInfoTables() {
-    do {
-        let enInfoTable = Table(enInfoTableName)
-        let dbDifficulty = Expression<Int>("difficulty")
-        let dbIds = Expression<String>("ids")
-        try dbW.run(enInfoTable.drop(ifExists: true))
-        try dbW.run(enInfoTable.create { t in
-            t.column(dbDifficulty, primaryKey: true)
-            t.column(dbIds)
-        })
 
-        var isAdded: [String: Bool] = [:]
-        var difficultyIds: [Int: [Int]] = [:]
-        for difficulty in 0 ... 1000 {
-            difficultyIds[difficulty] = []
-        }
-        for (id, row) in rows.enumerated() {
-            let eng = row[1]
-            if let isScoreOK = engOK[eng],
-               isScoreOK,
-               isAdded[eng] == nil,
-               let difficulty = engDifficulty[eng] {
-                isAdded[eng] = true
-                difficultyIds[difficulty]?.append(id)
-            }
-        }
-        var totalEnCount = 0
-        print("- en difficulty -")
-        for difficulty in 0 ... 1000 {
-            if let ids = difficultyIds[difficulty],
-               !ids.isEmpty {
-                let insertSql = enInfoTable.insert(
-                    dbDifficulty <- difficulty,
-                    dbIds <- encode(ids) ?? ""
-                )
-                print(difficulty, ids.count)
-                totalEnCount += ids.count
-                try dbW.run(insertSql)
-            }
-        }
-        print("totalEnCount: \(totalEnCount)")
-    } catch {
-        print("db error: \(error)")
+func addEnInfoTables() {
+    var isAdded: [String: Bool] = [:]
+    var difficultyIds: [Int: [Int]] = [:]
+    for difficulty in 0 ... 1000 {
+        difficultyIds[difficulty] = []
     }
+    for (id, row) in rows.enumerated() {
+        let eng = row[1]
+        if let isScoreOK = engOK[eng],
+           isScoreOK,
+           isAdded[eng] == nil,
+           let difficulty = engDifficulty[eng] {
+            isAdded[eng] = true
+            difficultyIds[difficulty]?.append(id)
+        }
+    }
+    var totalEnCount = 0
+    print("- en difficulty -")
+    for difficulty in 0 ... 1000 {
+        if let ids = difficultyIds[difficulty],
+           !ids.isEmpty {
+            let enInfo = RMEnInfo()
+            enInfo.difficulty = difficulty
+            enInfo.ids = encode(ids) ?? ""
+            print(difficulty, ids.count)
+            totalEnCount += ids.count
+        }
+    }
+    print("totalEnCount: \(totalEnCount)")
 }
 func runAll() {
     createWritableDB()
@@ -375,6 +301,7 @@ func runAll() {
     addStringToIdTable()
     addJpInfoTables()
     addEnInfoTables()
+    print("fileURL: \(realm.configuration.fileURL!)")
 }
 
 runAll()
