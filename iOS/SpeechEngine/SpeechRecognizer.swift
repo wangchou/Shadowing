3f74106d45936bcbd75b352551f9e2f0506cde47
Wangchou Lu <kindalu@gmail.com>
2020-12-03 15:14:14 +0800

    fix simulator crash

--- iOS/SpeechEngine/SpeechRecognizer.swift
+++ iOS/SpeechEngine/SpeechRecognizer.swift
@@ -194,7 +194,7 @@ class SpeechRecognizer: NSObject {
         postEvent(.listenStarted, string: "")
         Timer.scheduledTimer(withTimeInterval: stopAfterSeconds, repeats: false) { [weak self] _ in
             let fakeSuffix = ["", "", "西宮", "はは"]
-            let fakeSaidString = context.targetString + fakeSuffix[Int.random(in: 0 ..< fakeSuffix.count)]
+            let fakeSaidString = context.sentences[0].origin + fakeSuffix[Int.random(in: 0 ..< fakeSuffix.count)]
 
             self?.promise.fulfill(fakeSaidString)
         }
